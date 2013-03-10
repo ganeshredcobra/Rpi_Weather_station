c6da906f7a07e7d5c6d8df36d846872d6296ab9e
Ganesh <ganeshredcobra@gmail.com>
2013-03-11 01:35:28 +0530

    Time stamping the serial value

---
+++ python_scripts/timestamp.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+from os import fork, chdir, setsid, umask
+from sys import exit
+import serial,os
+import time
+from datetime import datetime
+
+
+
+
+def main():
+
+   def timestamp():
+	FORMAT = '%Y-%m-%d;%H-%M-%S'
+	STAMP = '%s' % (datetime.now().strftime(FORMAT))
+	return STAMP
+   time.sleep(120)
+   ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
+   while True:
+       #ser.flush()
+       data=ser.readline()
+       #print data
+       TIME=timestamp()
+       if data!='':
+           file=open('/var/www/serial.txt','a')
+           file.write('%s: '%TIME+data+'\n')
+           #time.sleep(60)
+       #ser.flushInput()
+       #ser.flushOutput()
+
+
+
+# Dual fork hack to make process run as a daemon
+if __name__ == "__main__":
+      try:
+        pid = fork()
+        if pid > 0:
+          exit(0)
+      except OSError, e:
+        exit(1)
+
+      chdir("/")
+      setsid()
+      umask(0)
+
+      try:
+        pid = fork()
+        if pid > 0:
+          exit(0)
+      except OSError, e:
+        exit(1)
+
+main()
